9b2f89ed74ccdeddae02cbac946004a7328944f8
Thomas Deppisch <tommi_deppisch@gmx.de>
2020-05-21 20:28:53 +0200

    change to new font

--- resources/lookAndFeel/BinaryFonts.h
+++ resources/lookAndFeel/BinaryFonts.h
@@ -4,16 +4,16 @@
 
 namespace BinaryFonts
 {
-    extern const char*  AAMedium_ttf;
-    const int           AAMedium_ttfSize = 277920;
-
     extern const char*  terminator_ttf;
     const int           terminator_ttfSize = 17408;
 
-    extern const char*  AAUltraLight_ttf;
-    const int           AAUltraLight_ttfSize = 369224;
+    extern const char*   NunitoSansLight_ttf;
+    const int            NunitoSansLight_ttfSize = 89764;
 
-    extern const char*  AARegular_ttf;
-    const int           AARegular_ttfSize = 421096;
+    extern const char*   NunitoSansRegular_ttf;
+    const int            NunitoSansRegular_ttfSize = 91460;
+
+    extern const char*   NunitoSansSemiBold_ttf;
+    const int            NunitoSansSemiBold_ttfSize = 90708;
 
 }
