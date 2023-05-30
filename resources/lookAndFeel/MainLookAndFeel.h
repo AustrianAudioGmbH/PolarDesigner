185dbb8238797a6dc3117836d0bf34810c345246
Mikolaj <mikolaj@chromium.org>
2023-05-30 17:49:19 +0200

    Add main Look and feel class.

---
+++ resources/lookAndFeel/MainLookAndFeel.h
@@ -0,0 +1,24 @@
+/*
+  ==============================================================================
+
+    MainLookAndFeel.h
+    Created: 30 May 2023 4:54:48pm
+    Author:  Mikolaj Cikuj
+
+  ==============================================================================
+*/
+
+#pragma once
+
+class MainLookAndFeel : public LookAndFeel_V4
+{
+public:
+    const Colour mainBackground = Colour(24, 25, 27);
+
+    MainLookAndFeel()
+    {
+
+    }
+
+    ~MainLookAndFeel() {}
+};
