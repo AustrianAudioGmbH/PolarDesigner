afd5ffc46689e0c75a890f89c203e99634bce489
mikcik91 <38189071+mikcik91@users.noreply.github.com>
2023-06-23 13:37:48 +0200

    Add animated label class.

---
+++ resources/customComponents/AnimatedLabel.h
@@ -0,0 +1,61 @@
+/*
+  ==============================================================================
+
+    AnimatedLabel.h
+    Created: 23 Jun 2023 10:32:56am
+    Author:  mikol
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+
+//==============================================================================
+/*
+*/
+class AnimatedLabel  : public juce::Component
+{
+public:
+    AnimatedLabel()
+    {
+        // In your constructor, you should add any child components, and
+        // initialise any special settings that your component needs.
+
+    }
+
+    ~AnimatedLabel() override
+    {
+    }
+
+    void paint (juce::Graphics& g) override
+    {
+        /* This demo code just fills the component's background and
+           draws some placeholder text to get you started.
+
+           You should replace everything in this method with your own
+           drawing code..
+        */
+
+        g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));   // clear the background
+
+        g.setColour (juce::Colours::grey);
+        g.drawRect (getLocalBounds(), 1);   // draw an outline around the component
+
+        g.setColour (juce::Colours::white);
+        g.setFont (14.0f);
+        g.drawText (getTitle(), getLocalBounds(),
+                    juce::Justification::centred, true);   // draw some placeholder text
+    }
+
+    void resized() override
+    {
+        // This method is where you should set the bounds of any child
+        // components that your component contains..
+
+    }
+
+private:
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AnimatedLabel)
+};
