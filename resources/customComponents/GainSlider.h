fcb9c760ba44dc94e0f856ab5bd75d5f3dbbfa5d
Mikolaj <mikolaj@chromium.org>
2023-07-16 14:17:10 +0200

    Fix double click gain slider issue.

--- resources/customComponents/GainSlider.h
+++ resources/customComponents/GainSlider.h
@@ -38,12 +38,6 @@ public:
         repaint();
     }
 
-    void mouseUp(const MouseEvent& event) override
-    {
-        setTextBoxStyle(Slider::TextBoxLeft, true, -1, -1);
-        repaint();
-    }
-
     void mouseExit(const MouseEvent& event) override
     {
         setTextBoxStyle(Slider::NoTextBox, true, -1, -1);
