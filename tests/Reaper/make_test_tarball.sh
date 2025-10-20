e7d84af667fa4607b963826cfd001ea1a1c80c9c
Jay Vaughan <jay.vaughan@austrianaudio.com>
2025-10-20 12:55:45 +0200

    make_test_tarball.sh: Use local path

--- tests/Reaper/make_test_tarball.sh
+++ tests/Reaper/make_test_tarball.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-echo "Making a Test tarball"
+echo "Making a Test tarball for the v2_vs_v3 test process..."
 tar cvf ~/Desktop/Tests/PolarDesigner_v2_vs_v3_TEST.tar Backups/ peaks/ Media/OC818-AH-\ Drums\ 1.wav Media/PolardesignerTestfile_TerminationControl_48kHz.wav Media/sine440.wav PolarDesigner_v2_vs_v3.RPP Scripts/
+ls -alF "*.tar"
+
