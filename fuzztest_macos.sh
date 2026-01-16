900d22a15ab8d8d3981b82654c9879a6a4cb588b
Sebastian Grill <sebastian.grill@atmoky.com>
2026-01-16 17:10:00 +0100

    chore: cleanup some old scripts that are no longer needed

--- fuzztest_macos.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Fuzz testing ... will repeatedly run pluginval on PolarDesigner3 until interrupted.  See /tmp/ for results*txt files."
-
-sleep 3
-
-while true ; do ~/Documents/Development/Plugins/pluginval/cmake-build-debug/pluginval_artefacts/Debug/pluginval.app/Contents/MacOS/pluginval --validate ~/Library/Audio/Plug-Ins/Components/PolarDesigner3.component --strictness-level 10 --validate-in-process | tee /tmp/results`date +%Y%m%d_%H%M%S`.txt ; echo "Sleeping 3 seconds..."; sleep 3 ; done
-
-
