99c049ebf254137d3261ebb184159e2743f65de0
Sebastian Grill <sebastian.grill@atmoky.com>
2026-01-16 17:10:00 +0100

    chore: cleanup some old scripts that are no longer needed

--- auv3find.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-mdfind "kMDItemContentType == 'com.apple.application-bundle'"
-mdfind "kMDItemContentType == 'com.apple.audio.unit' && kMDItemFSName == '*.component'"
--- fuzztest_linux.sh
+++
@@ -1,4 +0,0 @@
-
-#!/bin/bash
-
-./modules/pluginval/build/pluginval_artefacts/Debug/pluginval --validate ~/.vst3/PolarDesigner3.vst3/ --strictness-level 10 --validate-in-process 
