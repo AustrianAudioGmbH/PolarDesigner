7c576da0eb32b67f380b399ae19401e43922918f
Sebastian Grill <sebastian.grill@mailbox.org>
2026-07-16 22:08:39 +0200

    feat: implements optimized SpinLock

---
+++ source/SpinLock.hpp
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <array>
+#include <atomic>
+#include <emmintrin.h>
+#include <thread>
+
+namespace threadsafety
+{
+
+// TODO: this won't compile on ARM
+class SpinLock
+{
+public:
+    void lock()
+    {
+        static constexpr std::array iterations = { 5, 10, 3000 };
+
+        for (auto i = 0; i < iterations[0]; ++i)
+            if (tryLock())
+                return;
+
+        for (auto i = 0; i < iterations[1]; ++i)
+        {
+            if (tryLock())
+                return;
+
+            _mm_pause();
+        }
+
+        while (true)
+        {
+            for (auto i = 0; i < iterations[1]; ++i)
+            {
+                if (tryLock())
+                    return;
+
+                _mm_pause();
+                _mm_pause();
+                _mm_pause();
+                _mm_pause();
+                _mm_pause();
+                _mm_pause();
+                _mm_pause();
+                _mm_pause();
+                _mm_pause();
+                _mm_pause();
+            }
+
+            std::this_thread::yield();
+        }
+    }
+
+    bool tryLock() noexcept { return ! flag.test_and_set (std::memory_order_acquire); }
+
+    void unlock() noexcept { flag.clear (std::memory_order_relaxed); }
+
+private:
+    std::atomic_flag flag = ATOMIC_FLAG_INIT;
+};
+} // namespace threadsafety
